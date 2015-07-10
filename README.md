1a6a0c613a3570fccec105f6f773662ff8f512ac
SophGits <sophie.jiffy@gmail.com>
2015-07-10 21:49:06 +0100

    Create project structure and write gulp tasks for precompilation.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### Fact Keeper
+
+#### Get started
+* Install dependencies: `npm install`
+* Compile CSS and JS: `gulp`
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+
+gulp.task('default', ['watch']);
+
+gulp.task('minify-scripts', function() {
+  return gulp.src([
+    "assets/scripts/jquery-2.1.4.min.js",
+    "assets/scripts/script.js"
+    ])
+  .pipe(concat('script.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./assets/scripts'));
+});
+
+gulp.task('minify-less', function() {
+  return gulp.src('./assets/styles/style.less')
+  .pipe(less())
+  .pipe(minifyCSS())
+  .pipe(concat('style.min.css'))
+  .pipe(gulp.dest('./assets/styles/'));
+});
+
+// gulp.task('fonts', function() {
+//   return gulp.src('assets/webfontkit/*')
+//   .pipe(gulp.dest('./dist/assets/styles'));
+// });
+
+gulp.task('watch', function(){
+  gulp.watch('assets/styles/*.less', ['minify-less']);
+  gulp.watch('assets/scripts/*.js', ['minify-scripts']);
+  // gulp.watch('assets/webfontkit/*', ['fonts']);
+});
