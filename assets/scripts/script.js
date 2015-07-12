f3943b01948ac92af9c29bb573fbcb10af32a6dc
SophGits <sophie.jiffy@gmail.com>
2015-07-12 16:22:58 +0100

    Sketch out ideas for JSON structure. Link local .json as script file for now.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,11 @@
 $(document).ready( function() {
   console.log("Script file here.");
+
+  if(!!facts) {
+    console.log(facts);
+    $.each(facts, function(index, fact) {
+      console.log("\n" + index + ": " + fact.question + "\n");
+    })
+  }
+
 });
