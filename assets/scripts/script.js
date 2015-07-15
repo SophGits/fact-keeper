6125bff419cceb8c4d9b4e8d7950761c23f194be
SophGits <sophie.jiffy@gmail.com>
2015-07-15 22:14:24 +0100

    Move shuffle function to separate file. Add basic functionality for cycling through a set of questions - and resetting on completion.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,17 +3,31 @@ document.onreadystatechange = function () {
     return;
   }
 
+  // initialise fresh series of questions
+  window.series = {
+    "status": "new"
+  };
+
+  series.totalQuestions = 5;
+
   var randomButton = document.getElementById("random-question");
   randomButton.onclick = displayRandomQuestion;
 
+  series.nextButton = document.getElementById("next-question");
+  series.nextButton.onclick = displayNextQuestion;
+
   // set listener for answers
   var answersList = document.getElementsByClassName("answers-list")[0];
+  setAnswerSelectListener( answersList );
+
+}
+
+function setAnswerSelectListener( answersList ) {
   if( answersList.addEventListener ) {
     answersList.addEventListener( 'click', toggleAnswerSelect, false );
   } else if ( answersList.attachEvent ) {
     answersList.attachEvent( 'onclick', toggleAnswerSelect );
   }
-
 }
 
 function toggleAnswerSelect(e) {
@@ -58,6 +72,43 @@ function displayRandomQuestion() {
   });
 }
 
+function displayNextQuestion() {
+  if( window.series.status === "new" ) {
+
+    series["status"] = "in progress";
+    series.nextButton.value = "Next question (2 of " + series.totalQuestions + ")" ;
+    document.getElementById("total").innerHTML = series.totalQuestions;
+    series.nextButton.className = series.nextButton.className + " active";
+
+    // display question 1 of 3 | progress
+    updateProgressCounter(1);
+    displayRandomQuestion(); // starting question
+    series.count = 1;
+  } else {
+    displayRandomQuestion();
+    series.count += 1;
+    updateProgressCounter(series.count);
+    series.nextButton.value = "Next question (" + parseInt(series.count + 1) + " of " + series.totalQuestions + ")";
+    // debugger
+    if (  series.count === series.totalQuestions ) {
+      series.nextButton.value = "Submit question " + series.count + " of " + series.totalQuestions;
+    } else if (  series.count === series.totalQuestions +1 ) {
+      updateProgressCounter(0);
+      clearExistingAnswers();
+      series.nextButton.className = series.nextButton.className.split(' ').filter( function( className ) {
+        className != "active";
+      }).join(' ');
+      series.nextButton.value = "New Set of 5 Questions";
+      series.count = 0;
+      series.status = "new";
+    }
+  }
+}
+
+function updateProgressCounter( score ) {
+  document.getElementById("current-score").innerHTML = score;
+}
+
 function putAnswerNodesInList( answers, type ) {
   // console.log(arguments);
   answers.forEach( function( item )  {
@@ -71,30 +122,14 @@ function putAnswerNodesInList( answers, type ) {
 }
 
 function clearExistingAnswers() {
+  var questionSection = document.getElementsByClassName("question-section")[0];
+  questionSection = questionSection.querySelectorAll("header")[0];
+  questionSection.innerHTML = "Start again";
+
+
   var answersList = document.getElementsByClassName("answers-list")[0];
   while (answersList.firstChild) {
     answersList.removeChild(answersList.firstChild);
   }
   return answersList;
 }
-
-// Knoth Shuffle, from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
